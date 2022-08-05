0f81d85086c1d3985a033200b49f87d855d547dd
mattverse <mattpark1028@gmail.com>
2022-08-06 00:20:09 +0900

    Add querier

--- packages/bindings/src/querier.rs
+++ packages/bindings/src/querier.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::{QuerierWrapper, QueryRequest, StdResult};
 
-use crate::query::{FullDenomResponse, OsmosisQuery};
+use crate::query::{FullDenomResponse, OsmosisQuery, ArithmeticTwapResponse, ArithmeticTwapToNowResponse};
 
 /// This is a helper wrapper to easily use our custom queries
 pub struct OsmosisQuerier<'a> {
@@ -24,4 +24,41 @@ impl<'a> OsmosisQuerier<'a> {
         let request: QueryRequest<OsmosisQuery> = OsmosisQuery::into(full_denom_query);
         self.querier.query(&request)
     }
+
+    pub fn arithmetic_twap(
+        &self,
+        id: u64,
+        quote_asset_denom: String,
+        base_asset_denom: String,
+        start_time: i64,
+        end_time: i64,
+    ) -> StdResult<ArithmeticTwapResponse> {
+        let arithmetic_twap_query = OsmosisQuery::ArithmeticTwap {
+            id,
+            quote_asset_denom,
+            base_asset_denom,
+            start_time,
+            end_time,
+        };
+        let request: QueryRequest<OsmosisQuery> = OsmosisQuery::into(arithmetic_twap_query);
+        self.querier.query(&request)
+    }
+
+    pub fn arithmetic_twap_to_now(
+        &self,
+        id: u64,
+        quote_asset_denom: String,
+        base_asset_denom: String,
+        start_time: i64,
+    ) -> StdResult<ArithmeticTwapToNowResponse> {
+        let arithmetic_twap_to_now_query = OsmosisQuery::ArithmeticTwapToNow {
+            id,
+            quote_asset_denom,
+            base_asset_denom,
+            start_time,
+        };
+        let request: QueryRequest<OsmosisQuery> = OsmosisQuery::into(arithmetic_twap_to_now_query);
+        self.querier.query(&request)
+    }
+
 }
