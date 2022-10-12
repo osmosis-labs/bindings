2339117f8a4cbe83d2cbd7709996cd09565d4a6f
Jakub Bogucki <jakub@confio.gmbh>
2022-10-12 21:52:51 +0200

    Update contracts to cosmwasm-std 1.1, cw-plus 0.15 and introduce schema-derive

--- contracts/twap/src/msg.rs
+++ contracts/twap/src/msg.rs
@@ -1,13 +1,13 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::Decimal;
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[cw_serde]
 pub struct InstantiateMsg {}
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
+#[derive(QueryResponses)]
 pub enum QueryMsg {
+    #[returns(GetArithmeticTwapResponse)]
     GetArithmeticTwap {
         id: u64,
         quote_asset_denom: String,
@@ -15,6 +15,7 @@ pub enum QueryMsg {
         start_time: i64,
         end_time: i64,
     },
+    #[returns(GetArithmeticTwapToNowResponse)]
     GetArithmeticTwapToNow {
         id: u64,
         quote_asset_denom: String,
@@ -24,12 +25,12 @@ pub enum QueryMsg {
 }
 
 // We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[cw_serde]
 pub struct GetArithmeticTwapResponse {
     pub twap: Decimal,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[cw_serde]
 pub struct GetArithmeticTwapToNowResponse {
     pub twap: Decimal,
 }
